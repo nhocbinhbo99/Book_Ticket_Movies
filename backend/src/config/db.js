ee0f3f278e47a76291442436607a4f881a536c7e
ThanhCongHoang <congblade92@gmail.com>
2026-04-14 00:47:52 +0700

    remove node_modules

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async( ) =>{
      try {
-        await mongoose.connect(
-          "mongodb+srv://tqatulen3102005_db_user:Tqa3102005@cluster0.gq27zli.mongodb.net/?appName=Cluster0"  
-        );
-        console.log("kien ket thanh cong");
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("lien ket thanh cong");
 
      } catch (error) {
         console.log("loi ket noi scdl:",error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,22 @@
-import express, { request, response } from 'express';
-import taskRoute from './routes/tasksRouters.js';
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import taskRoute from "./routes/tasksRouters.js";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use("/api/tasks",taskRoute)
-
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("TicketFlix backend is running");
+});
+app.use("/api/tasks", taskRoute);
 connectDB();
-
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
